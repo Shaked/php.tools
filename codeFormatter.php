0dcbf96f9c132cd51be41b5a4ca1cade4f3db457
Carlos C <uldericofilho@gmail.com>
2014-04-11 10:33:21 +0200

    Update help

--- codeFormatter.php
+++ codeFormatter.php
@@ -61,6 +61,7 @@ class CodeFormatter {
 		"LINE_BEFORE_CURLY_FUNCTION" => false,
 		"LINE_BEFORE_FUNCTION" => false,
 		"ORDER_USE" => true,
+		"REMOVE_UNUSED_USE_STATEMENTS" => true,
 		"SPACE_AFTER_COMMA" => true,
 		"SPACE_AFTER_IF" => true,
 		"SPACE_AROUND_ARITHMETIC" => false,
@@ -87,20 +88,20 @@ class CodeFormatter {
 	private $code = '';
 	private $ptr = 0;
 	private $tkns = 0;
-	private function orderUseClauses($source = ''){
+	private function orderUseClauses($source = '') {
 		$use_stack = [];
 		$tokens = token_get_all($source);
 		$new_tokens = [];
-		while(list(, $token) = each($tokens)){
+		while (list(, $token) = each($tokens)) {
 			list($id, $text) = $this->get_token($token);
-			if(T_CLASS == $id || T_FUNCTION == $id){
+			if (T_CLASS == $id || T_FUNCTION == $id) {
 				break;
-			}elseif(T_USE == $id){
+			} elseif (T_USE == $id) {
 				$use_item = $text;
-				while(list(, $token) = each($tokens)){
+				while (list(, $token) = each($tokens)) {
 					list($id, $text) = $this->get_token($token);
 					$use_item .= $text;
-					if(ST_SEMI_COLON == $id){
+					if (ST_SEMI_COLON == $id) {
 						break;
 					}
 				}
@@ -109,26 +110,50 @@ class CodeFormatter {
 			}
 			$new_tokens[] = $token;
 		}
-		sort($use_stack);
+		natcasesort($use_stack);
+		$alias_list = [];
+		$alias_count = [];
+		foreach ($use_stack as $use) {
+			if (false !== stripos($use, ' as ')) {
+				$alias = substr(strstr($use, ' as '), strlen(' as '),-1);
+			} else {
+				$alias = basename(str_replace('\\', '/', trim(substr($use, strlen('use'),-1))));
+			}
+			$alias_list[$alias] = $use;
+			$alias_count[$alias] = 0;
+		}
 		$return = '';
-		foreach($new_tokens as $token){
-			if($token instanceof SurrogateToken){
+		foreach ($new_tokens as $token) {
+			if ($token instanceof SurrogateToken) {
 				$return .= array_shift($use_stack);
-			}elseif(is_array($token)){
-				$return .= $token[1];
-			}else{
-				$return .= $token;
+			} else {
+				list($id, $text) = $this->get_token($token);
+				if (T_STRING == $id && isset($alias_list[$text])) {
+					$alias_count[$text]++;
+				}
+				$return .= $text;
 			}
 		}
 		prev($tokens);
-		while(list(, $token) = each($tokens)){
+		while (list(, $token) = each($tokens)) {
 			list($id, $text) = $this->get_token($token);
+			if (T_STRING == $id && isset($alias_list[$text])) {
+				$alias_count[$text]++;
+			}
 			$return .= $text;
+		}
+		if ($this->options['REMOVE_UNUSED_USE_STATEMENTS']) {
+			$unused_import = array_keys(array_filter($alias_count, function($v) {
+				return 0 == $v;
+			}));
+			foreach ($unused_import as $v) {
+				$return = str_replace($alias_list[$v], null, $return);
+			}
 		}
 		return $return;
 	}
 	public function formatCode($source = '') {
-		if($this->options['ORDER_USE']){
+		if ($this->options['ORDER_USE']) {
 			$source = $this->orderUseClauses($source);
 		}
 		$this->tkns = token_get_all($source);
@@ -154,7 +179,7 @@ class CodeFormatter {
 		foreach ($this->tkns as $index => $token) {
 			list($id, $text) = $this->get_token($token);
 			$this->ptr = $index;
-			if ($halt_parser && $id!=ST_QUOTE) {
+			if ($halt_parser && $id != ST_QUOTE) {
 				$this->append_code($text, false);
 				continue;
 			}
@@ -170,33 +195,33 @@ class CodeFormatter {
 						$curly_open = false;
 						$this->append_code(trim($text));
 					} else {
-						if (($in_break || $this->is_token(ST_CURLY_CLOSE)) && $switch_level>0 && $switch_arr["l".$switch_level]>0 && $switch_arr["s".$switch_level]==$this->indent-2) {
+						if (($in_break || $this->is_token(ST_CURLY_CLOSE)) && $switch_level > 0 && $switch_arr["l".$switch_level] > 0 && $switch_arr["s".$switch_level] == $this->indent-2) {
 							if ($this->options["INDENT_CASE"]) {
 								$this->set_indent(-1);
 							}
 							$switch_arr["l".$switch_level]--;
 							$switch_arr["c".$switch_level]--;
 						}
-						while ($switch_level>0 && $switch_arr["l".$switch_level]==0 && $this->options["INDENT_CASE"]) {
+						while ($switch_level > 0 && $switch_arr["l".$switch_level] == 0 && $this->options["INDENT_CASE"]) {
 							unset($switch_arr["s".$switch_level]);
 							unset($switch_arr["c".$switch_level]);
 							unset($switch_arr["l".$switch_level]);
 							$switch_level--;
-							if ($switch_level>0) {
+							if ($switch_level > 0) {
 								$switch_arr["l".$switch_level]--;
 							}
 							$this->set_indent(-1);
 							$this->append_code($this->get_crlf_indent().$text.$this->get_crlf_indent());
 							$text = '';
 						}
-						if ($text!='') {
+						if ($text != '') {
 							$this->set_indent(-1);
 							$this->append_code($this->get_crlf_indent().$text.$this->get_crlf_indent());
 						}
 					}
 					break;
 				case ST_SEMI_COLON:
-					if (($in_break || $this->is_token(ST_CURLY_CLOSE)) && $switch_level>0 && $switch_arr["l".$switch_level]>0 && $switch_arr["s".$switch_level]==$this->indent-2) {
+					if (($in_break || $this->is_token(ST_CURLY_CLOSE)) && $switch_level > 0 && $switch_arr["l".$switch_level] > 0 && $switch_arr["s".$switch_level] == $this->indent-2) {
 						if ($this->options["INDENT_CASE"]) {
 							$this->set_indent(-1);
 						}
@@ -208,10 +233,10 @@ class CodeFormatter {
 						$in_concat = false;
 					}
 					$this->append_code($text.$this->get_crlf($this->options["LINE_AFTER_BREAK"] && $in_break).$this->get_crlf_indent($in_for));
-					while ($if_pending>0) {
+					while ($if_pending > 0) {
 						$text = $this->options["ADD_MISSING_BRACES"]?"}":"";
 						$this->set_indent(-1);
-						if ($text!="") {
+						if ($text != "") {
 							$this->append_code($this->get_crlf_indent().$text.$this->get_crlf_indent());
 						} else {
 							$this->append_code($this->get_crlf_indent());
@@ -221,7 +246,7 @@ class CodeFormatter {
 							break;
 						}
 					}
-					if ($this->for_idx==0) {
+					if ($this->for_idx == 0) {
 						$in_for = false;
 					}
 					$in_break = false;
@@ -240,10 +265,10 @@ class CodeFormatter {
 					$this->append_code($text);
 					break;
 				case ST_PARENTHESES_OPEN:
-					if ($if_level>0) {
+					if ($if_level > 0) {
 						$if_parentheses["i".$if_level]++;
 					}
-					if ($array_level>0) {
+					if ($array_level > 0) {
 						$arr_parentheses["i".$array_level]++;
 						if ($this->is_token(array(T_ARRAY), true) && !$this->is_token(ST_PARENTHESES_CLOSE)) {
 							$this->set_indent(+1);
@@ -257,10 +282,10 @@ class CodeFormatter {
 					$space_after_t_use = false;
 					break;
 				case ST_PARENTHESES_CLOSE:
-					if ($array_level>0) {
+					if ($array_level > 0) {
 						$arr_parentheses["i".$array_level]--;
-						if ($arr_parentheses["i".$array_level]==0) {
-							$comma = substr(trim($this->code),-1)!="," && $this->options['VERTICAL_ARRAY']?",":"";
+						if ($arr_parentheses["i".$array_level] == 0) {
+							$comma = substr(trim($this->code),-1) != "," && $this->options['VERTICAL_ARRAY']?",":"";
 							$this->set_indent(-1);
 							$this->append_code($comma.$this->get_crlf_indent().$text.$this->get_crlf_indent());
 							unset($arr_parentheses["i".$array_level]);
@@ -269,13 +294,13 @@ class CodeFormatter {
 						}
 					}
 					$this->append_code($this->get_space($this->options["SPACE_INSIDE_PARENTHESES"]).$text.$this->get_space($this->options["SPACE_OUTSIDE_PARENTHESES"]));
-					if ($if_level>0) {
+					if ($if_level > 0) {
 						$if_parentheses["i".$if_level]--;
-						if ($if_parentheses["i".$if_level]==0) {
+						if ($if_parentheses["i".$if_level] == 0) {
 							if (!$this->is_token(ST_CURLY_OPEN) && !$this->is_token(ST_SEMI_COLON)) {
 								$text = $this->options["ADD_MISSING_BRACES"]?"{":"";
 								$this->set_indent(+1);
-								$this->append_code((!$this->options["LINE_BEFORE_CURLY"] || $text==""?' ':$this->get_crlf_indent(false,-1)).$text.$this->get_crlf_indent());
+								$this->append_code((!$this->options["LINE_BEFORE_CURLY"] || $text == ""?' ':$this->get_crlf_indent(false,-1)).$text.$this->get_crlf_indent());
 								$if_pending++;
 							}
 							unset($if_parentheses["i".$if_level]);
@@ -284,7 +309,7 @@ class CodeFormatter {
 					}
 					break;
 				case ST_COMMA:
-					if ($array_level>0 && 0 == $bracket_level) {
+					if ($array_level > 0 && 0 == $bracket_level) {
 						$this->append_code($text.$this->get_crlf_indent($in_for));
 					} else {
 						$this->append_code($text.$this->get_space($this->options["SPACE_AFTER_COMMA"]));
@@ -361,7 +386,7 @@ class CodeFormatter {
 					$this->append_code($this->get_space($condition).$text.$this->get_space($condition));
 					break;
 				case ST_COLON:
-					if ($switch_level>0 && $switch_arr["l".$switch_level]>0 && $switch_arr["c".$switch_level]<$switch_arr["l".$switch_level]) {
+					if ($switch_level > 0 && $switch_arr["l".$switch_level] > 0 && $switch_arr["c".$switch_level] < $switch_arr["l".$switch_level]) {
 						$switch_arr["c".$switch_level]++;
 						if ($this->options["INDENT_CASE"]) {
 							$this->set_indent(+1);
@@ -374,7 +399,7 @@ class CodeFormatter {
 						}
 						$this->append_code($this->get_space($condition).$text.$this->get_space($condition));
 					}
-					if (($in_break || $this->is_token(ST_CURLY_CLOSE)) && $switch_level>0 && $switch_arr["l".$switch_level]>0) {
+					if (($in_break || $this->is_token(ST_CURLY_CLOSE)) && $switch_level > 0 && $switch_arr["l".$switch_level] > 0) {
 						if ($this->options["INDENT_CASE"]) {
 							$this->set_indent(-1);
 						}
@@ -420,7 +445,7 @@ class CodeFormatter {
 				case T_SWITCH:
 					$space_after = $this->options["SPACE_AFTER_IF"];
 					$this->append_code($text.$this->get_space($space_after), false);
-					if ($id==T_SWITCH) {
+					if ($id == T_SWITCH) {
 						$switch_level++;
 						$switch_arr["s".$switch_level] = $this->indent;
 						$switch_arr["l".$switch_level] = 0;
@@ -466,13 +491,13 @@ class CodeFormatter {
 						$pad = $this->tkns[$index-1][1];
 						$i = strlen($pad)-1;
 						$k = "";
-						while (substr($pad, $i, 1)!="\n" && substr($pad, $i, 1)!="\r" && $i>=0) {
+						while (substr($pad, $i, 1) != "\n" && substr($pad, $i, 1) != "\r" && $i >= 0) {
 							$k .= substr($pad, $i--, 1);
 						}
 						$text = preg_replace("/\r?\n$k/", $this->get_crlf_indent(), $text);
 					}
-					$after = $id==(T_COMMENT && preg_match("/^\/\//", $text))?$this->options["LINE_AFTER_COMMENT"]:$this->options["LINE_AFTER_COMMENT_MULTI"];
-					$before = $id==(T_COMMENT && preg_match("/^\/\//", $text))?$this->options["LINE_BEFORE_COMMENT"]:$this->options["LINE_BEFORE_COMMENT_MULTI"];
+					$after = $id == (T_COMMENT && preg_match("/^\/\//", $text))?$this->options["LINE_AFTER_COMMENT"]:$this->options["LINE_AFTER_COMMENT_MULTI"];
+					$before = $id == (T_COMMENT && preg_match("/^\/\//", $text))?$this->options["LINE_BEFORE_COMMENT"]:$this->options["LINE_BEFORE_COMMENT_MULTI"];
 					if ($prev = $this->is_token(ST_CURLY_OPEN, true, $index, true)) {
 						$before = $before && !$this->is_token_lf(true, $prev);
 					}
@@ -506,10 +531,10 @@ class CodeFormatter {
 					$redundant = "";
 					if ($this->options["KEEP_REDUNDANT_LINES"]) {
 						$lines = preg_match_all("/\r?\n/", $text, $matches);
-						$lines = $lines>0?$lines-1:0;
-						$redundant = $lines>0?str_repeat($this->new_line, $lines):"";
+						$lines = $lines > 0?$lines-1:0;
+						$redundant = $lines > 0?str_repeat($this->new_line, $lines):"";
 						$current_indent = $this->get_indent();
-						if (substr($this->code, strlen($current_indent)*-1)==$current_indent && $lines>0) {
+						if (substr($this->code, strlen($current_indent)*-1) == $current_indent && $lines > 0) {
 							$redundant .= $current_indent;
 						}
 					}
@@ -527,7 +552,7 @@ class CodeFormatter {
 					if ($this->options["VERTICAL_ARRAY"]) {
 						$next = $this->is_token(array(T_DOUBLE_ARROW), true);
 						$next |= $this->is_token(ST_EQUAL, true);
-						$next |= $array_level>0;
+						$next |= $array_level > 0;
 						if ($next) {
 							$next = $this->is_token(ST_PARENTHESES_OPEN, false, $index, true);
 							if ($next) {
@@ -563,7 +588,7 @@ class CodeFormatter {
 					break;
 				case T_CASE:
 				case T_DEFAULT:
-					if ($switch_arr["l".$switch_level]>0 && $this->options["INDENT_CASE"]) {
+					if ($switch_arr["l".$switch_level] > 0 && $this->options["INDENT_CASE"]) {
 						$switch_arr["c".$switch_level]--;
 						$this->set_indent(-1);
 						$this->append_code($this->get_crlf_indent().$text.$this->get_space());
@@ -621,7 +646,7 @@ class CodeFormatter {
 					if (!$this->is_token(ST_CURLY_OPEN) && !$this->is_token(array(T_IF))) {
 						$text = $this->options["ADD_MISSING_BRACES"]?"{":"";
 						$this->set_indent(+1);
-						$this->append_code((!$this->options["LINE_BEFORE_CURLY"] || $text==""?' ':$this->get_crlf_indent(false,-1)).$text.$this->get_crlf_indent());
+						$this->append_code((!$this->options["LINE_BEFORE_CURLY"] || $text == ""?' ':$this->get_crlf_indent(false,-1)).$text.$this->get_crlf_indent());
 						$if_pending++;
 					}
 					break;
@@ -649,11 +674,11 @@ class CodeFormatter {
 	private function get_crlf_indent($in_for = false, $increment = 0) {
 		if ($in_for) {
 			$this->for_idx++;
-			if ($this->for_idx>2) {
+			if ($this->for_idx > 2) {
 				$this->for_idx = 0;
 			}
 		}
-		if ($this->for_idx==0 || !$in_for) {
+		if ($this->for_idx == 0 || !$in_for) {
 			return $this->get_crlf().$this->get_indent($increment);
 		} else {
 			return $this->get_space($this->options["SPACE_INSIDE_FOR"]);
@@ -670,42 +695,42 @@ class CodeFormatter {
 	}
 	private function set_indent($increment) {
 		$this->indent += $increment;
-		if ($this->indent<0) {
+		if ($this->indent < 0) {
 			$this->indent = 0;
 		}
 	}
 	private function is_token($token, $prev = false, $i = 99999, $idx = false) {
-		if ($i==99999) {
+		if ($i == 99999) {
 			$i = $this->ptr;
 		}
 		if ($prev) {
-			while (--$i>=0 && is_array($this->tkns[$i]) && $this->tkns[$i][0]==T_WHITESPACE);
+			while (--$i >= 0 && is_array($this->tkns[$i]) && $this->tkns[$i][0] == T_WHITESPACE);
 		} else {
-			while (++$i<sizeof($this->tkns)-1 && is_array($this->tkns[$i]) && $this->tkns[$i][0]==T_WHITESPACE);
+			while (++$i < sizeof($this->tkns)-1 && is_array($this->tkns[$i]) && $this->tkns[$i][0] == T_WHITESPACE);
 		}
-		if (isset($this->tkns[$i]) && is_string($this->tkns[$i]) && $this->tkns[$i]==$token) {
+		if (isset($this->tkns[$i]) && is_string($this->tkns[$i]) && $this->tkns[$i] == $token) {
 			return $idx?$i:true;
 		} elseif (is_array($token) && is_array($this->tkns[$i])) {
 			if (in_array($this->tkns[$i][0], $token)) {
 				return $idx?$i:true;
-			} elseif ($prev && $this->tkns[$i][0]==T_OPEN_TAG) {
+			} elseif ($prev && $this->tkns[$i][0] == T_OPEN_TAG) {
 				return $idx?$i:true;
 			}
 		}
 		return false;
 	}
 	private function is_token_lf($prev = false, $i = 99999) {
-		if ($i==99999) {
+		if ($i == 99999) {
 			$i = $this->ptr;
 		}
 		if ($prev) {
 			$count = 0;
-			while (--$i>=0 && is_array($this->tkns[$i]) && $this->tkns[$i][0]==T_WHITESPACE && strpos($this->tkns[$i][1], "\n")===false);
+			while (--$i >= 0 && is_array($this->tkns[$i]) && $this->tkns[$i][0] == T_WHITESPACE && strpos($this->tkns[$i][1], "\n") === false);
 		} else {
 			$count = 1;
-			while (++$i<sizeof($this->tkns) && is_array($this->tkns[$i]) && $this->tkns[$i][0]==T_WHITESPACE && strpos($this->tkns[$i][1], "\n")===false);
+			while (++$i < sizeof($this->tkns) && is_array($this->tkns[$i]) && $this->tkns[$i][0] == T_WHITESPACE && strpos($this->tkns[$i][1], "\n") === false);
 		}
-		if (is_array($this->tkns[$i]) && preg_match_all("/\r?\n/", $this->tkns[$i][1], $matches)>$count) {
+		if (is_array($this->tkns[$i]) && preg_match_all("/\r?\n/", $this->tkns[$i][1], $matches) > $count) {
 			return true;
 		}
 		return false;
@@ -720,11 +745,11 @@ class CodeFormatter {
 		foreach ($source as $k => $line) {
 			if (preg_match("/'quote[0-9]+'/", $line)) {
 				preg_match_all("/'quote([0-9]+)'/", $line, $holders);
-				for ($i = 0;$i<sizeof($holders[1]);$i++) {
+				for ($i = 0;$i < sizeof($holders[1]);$i++) {
 					$line = preg_replace("/".$holders[0][$i]."/", str_repeat(" ", strlen($quotes[0][$holders[1][$i]])), $line);
 				}
 			}
-			if (strpos($line, "=")>$pad_size) {
+			if (strpos($line, "=") > $pad_size) {
 				$pad_size = strpos($line, "=");
 			}
 			$position[$k] = strpos($line, "=");
@@ -749,19 +774,19 @@ class CodeFormatter {
 		$php_code = $blocks[0];
 		preg_match_all("/\/\*.*?\*\/|\/\/[^\n]*|#[^\n]|([\"'])[^\\\\]*?(?:\\\\.[^\\\\]*?)*?\\1/s", $php_code, $quotes);
 		$quotes[0] = array_values(array_unique($quotes[0]));
-		for ($i = 0;$i<sizeof($quotes[0]);$i++) {
+		for ($i = 0;$i < sizeof($quotes[0]);$i++) {
 			$patterns[] = "/".preg_quote($quotes[0][$i], '/')."/";
 			$holders[] = "'quote$i'";
 			$quotes[0][$i] = str_replace('\\\\', '\\\\\\\\', $quotes[0][$i]);
 		}
-		if (sizeof($holders)>0) {
+		if (sizeof($holders) > 0) {
 			$php_code = preg_replace($patterns, $holders, $php_code);
 		}
 		$php_code = preg_replace_callback("/(?:.+=.+[".$pad_chars."]\r?\n){".$this->block_size.",}/", array($this, "pad_operators"), $php_code);
-		for ($i = sizeof($holders)-1;$i>=0;$i--) {
+		for ($i = sizeof($holders)-1;$i >= 0;$i--) {
 			$holders[$i] = "/".$holders[$i]."/";
 		}
-		if (sizeof($holders)>0) {
+		if (sizeof($holders) > 0) {
 			$php_code = preg_replace($holders, $quotes[0], $php_code);
 		}
 		return $php_code;
@@ -774,4 +799,5 @@ class CodeFormatter {
 		}
 	}
 }
-class SurrogateToken {}
+class SurrogateToken {
+}
