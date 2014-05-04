56683afd2762457471322bbc57454299f467726e
Carlos C <uldericofilho@gmail.com>
2014-05-05 00:08:43 +0200

    Improving handling of comments

--- codeFormatter.php
+++ codeFormatter.php
@@ -1,20 +1,18 @@
 <?php
-/*
-# Inspired by http://phpstylist.sourceforge.net/
-#
-# Copyright (c) 2014, Carlos C
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-#
-# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+//Inspired by http://phpstylist.sourceforge.net/
+//
+//Copyright (c) 2014, Carlos C
+//All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//
+//1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//
+//2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//
+//3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 define("ST_AT", "@");
 define("ST_BRACKET_CLOSE", "]");
 define("ST_BRACKET_OPEN", "[");
@@ -556,10 +554,12 @@ class CodeFormatter {
 					$in_heredoc_context = true;
 					break;
 				case T_COMMENT:
-					if ('//' == substr($text, 0, 2)) {
-						$text = '/*'.trim(str_replace(['/*', '*/'], '', substr($text, 2))).'*/';
-					} elseif ('#' == substr($text, 0, 1)) {
-						$text = '/*'.trim(str_replace(['/*', '*/'], '', substr($text, 1))).'*/';
+					if ($this->is_token(ST_CURLY_OPEN) || $this->is_token(array(T_OBJECT_OPERATOR))) {
+						if ('//' == substr($text, 0, 2)) {
+							$text = '/*'.trim(str_replace(['/*', '*/'], '', substr($text, 2))).'*/';
+						} elseif ('#' == substr($text, 0, 1)) {
+							$text = '/*'.trim(str_replace(['/*', '*/'], '', substr($text, 1))).'*/';
+						}
 					}
 				case T_DOC_COMMENT:
 					if (is_array($this->tkns[$index-1])) {
