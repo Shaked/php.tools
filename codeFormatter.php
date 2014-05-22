2655423e9e4c08663ea81795e82946e11404eb21
Carlos C <uldericofilho@gmail.com>
2014-05-22 11:03:43 +0200

    Fixing colon block indent - incomplete

--- codeFormatter.php
+++ codeFormatter.php
@@ -294,32 +294,46 @@ class CodeFormatter {
 	private function reindent_colon_blocks($source) {
 		$this->tkns = token_get_all($source);
 		$this->code = '';
-		$case_level = 0;
+
+		$switch_level                      = 0;
+		$switch_curly_count                = [];
+		$switch_curly_count[$switch_level] = 0;
 		while (list($index, $token) = each($this->tkns)) {
 			list($id, $text) = $this->get_token($token);
 			$this->ptr = $index;
 			switch ($id) {
+				case T_SWITCH:
+					$switch_level++;
+					$switch_curly_count[$switch_level] = 0;
+					$this->append_code($text, false);
+					break;
+				case ST_CURLY_OPEN:
+					$switch_curly_count[$switch_level]++;
+					$this->append_code($text, false);
+					break;
+				case ST_CURLY_CLOSE:
+					$switch_curly_count[$switch_level]--;
+					if (0 == $switch_curly_count[$switch_level] && $switch_level > 0) {
+						$switch_level--;
+					}
+					$this->append_code($text, false);
+					break;
 				case T_DEFAULT:
 				case T_CASE:
 					$this->append_code($text, false);
-					$case_level = 1;
-					break;
-				case ST_CURLY_CLOSE:
-					$this->append_code($text, false);
-					$case_level = 0;
 					break;
 				default:
 					if (substr_count($text, $this->new_line) > 0 && !$this->is_token(array(T_CASE, T_DEFAULT)) && !$this->is_token(ST_CURLY_CLOSE)) {
-						$this->append_code($text.$this->get_indent($case_level), false);
+						$this->append_code($text.$this->get_indent($switch_level), false);
 					} elseif (substr_count($text, $this->new_line) > 0 && $this->is_token(array(T_CASE, T_DEFAULT))) {
-						$case_level = 0;
-						$this->append_code($text.$this->get_indent($case_level), false);
+						$this->append_code($text, false);
 					} else {
 						$this->append_code($text, false);
 					}
 					break;
 			}
 		}
+		die($this->code);
 
 		return $this->code;
 	}
