064e02e3f989e93bde3cf48c7e0303e8a12fe34d
Carlos C <uldericofilho@gmail.com>
2014-06-12 16:26:31 +0200

    align =>

--- codeFormatter.php
+++ codeFormatter.php
@@ -582,6 +582,124 @@ class CodeFormatter {
 
 		return $this->code;
 	}
+	private function reindent_double_arrow($source) {
+		$this->tkns = token_get_all($source);
+		$this->code              = '';
+		$in_objop_context        = 0;// 1 - indent, 2 - don't indent, so future auto-align takes place
+		$alignable_objop_counter = 0;
+		$printed_placeholder     = false;
+		$paren_count             = 0;
+		$bracket_count           = 0;
+		while (list($index, $token) = each($this->tkns)) {
+			list($id, $text) = $this->get_token($token);
+			$this->ptr = $index;
+			switch ($id) {
+				case ST_PARENTHESES_OPEN:
+					$paren_count++;
+					$this->append_code($text, false);
+					break;
+				case ST_PARENTHESES_CLOSE:
+					$paren_count--;
+					$this->append_code($text, false);
+					break;
+				case ST_BRACKET_OPEN:
+					$bracket_count++;
+					$this->append_code($text, false);
+					break;
+				case ST_BRACKET_CLOSE:
+					$bracket_count--;
+					$this->append_code($text, false);
+					break;
+				case T_DOUBLE_ARROW:
+					if (0 === $in_objop_context && ($this->has_ln_before() || $this->has_ln_prev_token())) {
+						$in_objop_context = 1;
+					} elseif (0 === $in_objop_context && !($this->has_ln_before() || $this->has_ln_prev_token())) {
+						$alignable_objop_counter++;
+						$in_objop_context = 2;
+					} elseif ($paren_count > 0) {
+						$in_objop_context = 0;
+					}
+					if (1 == $in_objop_context) {
+						$this->set_indent(+1);
+						$this->append_code($this->get_indent().$text, false);
+						$this->set_indent(-1);
+					} elseif (2 === $in_objop_context) {
+						$placeholder = '';
+						if (!$printed_placeholder) {
+							$placeholder = sprintf(self::ALIGNABLE_OBJOP, $alignable_objop_counter);
+							$printed_placeholder = true;
+						}
+						$this->append_code($placeholder.$text, false);
+					} else {
+						$this->append_code($text, false);
+					}
+					break;
+				case T_VARIABLE:
+					if (0 === $paren_count && 0 === $bracket_count && 0 !== $in_objop_context) {
+						$in_objop_context = 0;
+					}
+					$this->append_code($text, false);
+					break;
+				case ST_SEMI_COLON:
+					if (0 !== $in_objop_context) {
+						$in_objop_context = 0;
+					}
+					$this->append_code($text, false);
+					break;
+				default:
+					$this->append_code($text, false);
+					break;
+			}
+			if (substr_count($text, $this->new_line) > 0) {
+				$printed_placeholder = false;
+			}
+		}
+
+		for ($j = $alignable_objop_counter; $j > 0; $j--) {
+			$current_align_objop = sprintf(self::ALIGNABLE_OBJOP, $j);
+			if (substr_count($this->code, $current_align_objop) <= 1) {
+				$this->code = str_replace($current_align_objop, '', $this->code);
+				continue;
+			}
+
+			$lines = explode($this->new_line, $this->code);
+			$lines_with_objop = [];
+			$block_count      = 0;
+
+			foreach ($lines as $idx => $line) {
+				if (substr_count($line, $current_align_objop) > 0) {
+					$lines_with_objop[$block_count][] = $idx;
+				} else {
+					$block_count++;
+				}
+			}
+
+			$i = 0;
+			foreach ($lines_with_objop as $group) {
+				if (1 == sizeof($group)) {
+					continue;
+				}
+				$i++;
+				$farthest_objop = 0;
+				foreach ($group as $idx) {
+					$farthest_objop = max($farthest_objop, strpos($lines[$idx], $current_align_objop));
+				}
+				foreach ($group as $idx) {
+					$line = $lines[$idx];
+					$current_objop = strpos($line, $current_align_objop);
+					$delta = abs($farthest_objop-$current_objop);
+					if ($delta > 0) {
+						$line = str_replace($current_align_objop, str_repeat(' ', $delta).$current_align_objop, $line);
+						$lines[$idx] = $line;
+					}
+				}
+			}
+
+			$this->code = str_replace($current_align_objop, '', implode($this->new_line, $lines));
+		}
+
+		return $this->code;
+	}
 	private function two_commands_in_same_line($source) {
 		$lines = explode($this->new_line, $source);
 		foreach ($lines as $idx => $line) {
@@ -917,6 +1035,45 @@ class CodeFormatter {
 			}
 		}
 
+		$lines_with_double_arrow = [];
+		$block_count             = 0;
+
+		foreach ($lines as $idx => $line) {
+			if (1 == substr_count($line, '=>') &&
+				(
+					(0 == substr_count($line, '(') && 0 == substr_count($line, ')'))
+					 ||
+					(
+						0 != substr_count($line, '(') &&
+						strpos($line, '(') > strpos($line, '=>')
+					)
+				)
+			) {
+				$lines_with_double_arrow[$block_count][] = $idx;
+			} else {
+				$block_count++;
+			}
+		}
+
+		foreach ($lines_with_double_arrow as $group) {
+			if (1 == sizeof($group)) {
+				continue;
+			}
+			$farthest_double_arrow = 0;
+			foreach ($group as $idx) {
+				$farthest_double_arrow = max($farthest_double_arrow, strpos($lines[$idx], '=>'));
+			}
+			foreach ($group as $idx) {
+				$line = $lines[$idx];
+				$current_equals = strpos($line, '=>');
+				$delta = abs($farthest_double_arrow-$current_equals);
+				if ($delta > 0) {
+					$line = preg_replace('/=>/', str_repeat(' ', $delta).'=>', $line, 1);
+					$lines[$idx] = $line;
+				}
+			}
+		}
+
 		$lines_with_alignable_comments = [];
 		$block_count                   = 0;
 		foreach ($lines as $idx => $line) {
@@ -1032,7 +1189,7 @@ class CodeFormatter {
 					}
 				case T_DOUBLE_ARROW:
 					if ($this->is_token(array(T_CONSTANT_ENCAPSED_STRING, T_STRING, T_VARIABLE), true)) {
-						$this->append_code($this->get_space().$text.$this->get_space(), false);
+						$this->append_code($this->get_space().$text.$this->get_space());
 						break;
 					}
 				case T_PUBLIC:
