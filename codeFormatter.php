947eda468c2cdef6858be0c1af1729304b582cab
Carlos C <uldericofilho@gmail.com>
2014-07-11 15:11:11 +0200

    Lowercase to keywords - PSR-2 §2.5

--- codeFormatter.php
+++ codeFormatter.php
@@ -1685,6 +1685,101 @@ final class PSR2IndentWithSpace extends FormatterPass {
 	}
 }
 
+final class PSR2KeywordsLowerCase extends FormatterPass {
+	public function format($source) {
+		$this->tkns = token_get_all($source);
+		$this->code = '';
+		while (list($index, $token) = each($this->tkns)) {
+			list($id, $text) = $this->get_token($token);
+			$this->ptr       = $index;
+			switch ($id) {
+				case T_ABSTRACT:
+				case T_ARRAY:
+				case T_ARRAY_CAST:
+				case T_AS:
+				case T_BOOL_CAST:
+				case T_BREAK:
+				case T_CASE:
+				case T_CATCH:
+				case T_CLASS:
+				case T_CLONE:
+				case T_CONST:
+				case T_CONTINUE:
+				case T_DECLARE:
+				case T_DEFAULT:
+				case T_DO:
+				case T_DOUBLE_CAST:
+				case T_ECHO:
+				case T_ELSE:
+				case T_ELSEIF:
+				case T_EMPTY:
+				case T_ENCAPSED_AND_WHITESPACE:
+				case T_ENDDECLARE:
+				case T_ENDFOR:
+				case T_ENDFOREACH:
+				case T_ENDIF:
+				case T_ENDSWITCH:
+				case T_ENDWHILE:
+				case T_EVAL:
+				case T_EXIT:
+				case T_EXTENDS:
+				case T_FINAL:
+				case T_FINALLY:
+				case T_FOR:
+				case T_FOREACH:
+				case T_FUNCTION:
+				case T_GLOBAL:
+				case T_GOTO:
+				case T_IF:
+				case T_IMPLEMENTS:
+				case T_INCLUDE:
+				case T_INCLUDE_ONCE:
+				case T_INSTANCEOF:
+				case T_INSTEADOF:
+				case T_INT_CAST:
+				case T_INTERFACE:
+				case T_ISSET:
+				case T_LIST:
+				case T_LOGICAL_AND:
+				case T_LOGICAL_OR:
+				case T_LOGICAL_XOR:
+				case T_NAMESPACE:
+				case T_NEW:
+				case T_OBJECT_CAST:
+				case T_PRINT:
+				case T_PRIVATE:
+				case T_PUBLIC:
+				case T_PROTECTED:
+				case T_REQUIRE:
+				case T_REQUIRE_ONCE:
+				case T_RETURN:
+				case T_STATIC:
+				case T_STRING_CAST:
+				case T_SWITCH:
+				case T_THROW:
+				case T_TRAIT:
+				case T_TRY:
+				case T_UNSET:
+				case T_UNSET_CAST:
+				case T_USE:
+				case T_VAR:
+				case T_WHILE:
+				case T_XOR_EQUAL:
+				case T_YIELD:
+					$this->append_code(strtolower($text), false);
+					break;
+				default:
+					$lc_text = strtolower($text);
+					if ('true' == $lc_text || 'false' == $lc_text || 'null' == $lc_text) {
+						$text = $lc_text;
+					}
+					$this->append_code($text, false);
+					break;
+			}
+		}
+		return $this->code;
+	}
+}
 final class PSR2LnAfterNamespace extends FormatterPass {
 	public function format($source) {
 		$this->tkns = token_get_all($source);
@@ -1934,6 +2029,7 @@ class PsrDecorator {
 		$fmt->addPass(new PSR1ClassConstants());
 		$fmt->addPass(new PSR1MethodNames());
 
+		$fmt->addPass(new PSR2KeywordsLowerCase());
 		$fmt->addPass(new PSR2IndentWithSpace());
 		$fmt->addPass(new PSR2LnAfterNamespace());
 		$fmt->addPass(new PSR2CurlyOpenNextLine());
