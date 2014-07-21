b7c0d1bd4b5759a1465771ff451d546f8a107305
Carlos C <uldericofilho@gmail.com>
2014-07-21 14:33:55 +0200

    Fixing problem with align of T_EQUAL

--- codeFormatter.php
+++ codeFormatter.php
@@ -253,14 +253,19 @@ final class AlignDoubleArrow extends FormatterPass {
 final class AlignEquals extends FormatterPass {
 	const ALIGNABLE_EQUAL = "\x2 EQUAL%d \x3";
 	public function format($source) {
-		$this->tkns    = token_get_all($source);
-		$this->code    = '';
-		$paren_count   = 0;
-		$bracket_count = 0;
+		$this->tkns      = token_get_all($source);
+		$this->code      = '';
+		$paren_count     = 0;
+		$bracket_count   = 0;
+		$context_counter = 0;
 		while (list($index, $token) = each($this->tkns)) {
 			list($id, $text) = $this->get_token($token);
 			$this->ptr       = $index;
 			switch ($id) {
+				case T_FUNCTION:
+					$context_counter++;
+					$this->append_code($text, false);
+					break;
 				case ST_PARENTHESES_OPEN:
 					$paren_count++;
 					$this->append_code($text, false);
@@ -279,7 +284,7 @@ final class AlignEquals extends FormatterPass {
 					break;
 				case ST_EQUAL:
 					if (!$paren_count && !$bracket_count) {
-						$this->append_code(self::ALIGNABLE_EQUAL.$text, false);
+						$this->append_code(sprintf(self::ALIGNABLE_EQUAL, $context_counter).$text, false);
 						break;
 					}
 
@@ -289,40 +294,43 @@ final class AlignEquals extends FormatterPass {
 			}
 		}
 
-		$lines            = explode($this->new_line, $this->code);
-		$lines_with_objop = [];
-		$block_count      = 0;
+		for ($j = 0; $j <= $context_counter; $j++) {
+			$placeholder      = sprintf(self::ALIGNABLE_EQUAL, $j);
+			$lines            = explode($this->new_line, $this->code);
+			$lines_with_objop = [];
+			$block_count      = 0;
 
-		foreach ($lines as $idx => $line) {
-			if (substr_count($line, self::ALIGNABLE_EQUAL) > 0) {
-				$lines_with_objop[$block_count][] = $idx;
-			} else {
-				$block_count++;
-			}
-		}
-
-		$i = 0;
-		foreach ($lines_with_objop as $group) {
-			if (1 == sizeof($group)) {
-				continue;
-			}
-			$i++;
-			$farthest_objop = 0;
-			foreach ($group as $idx) {
-				$farthest_objop = max($farthest_objop, strpos($lines[$idx], self::ALIGNABLE_EQUAL));
-			}
-			foreach ($group as $idx) {
-				$line          = $lines[$idx];
-				$current_objop = strpos($line, self::ALIGNABLE_EQUAL);
-				$delta         = abs($farthest_objop-$current_objop);
-				if ($delta > 0) {
-					$line        = str_replace(self::ALIGNABLE_EQUAL, str_repeat(' ', $delta).self::ALIGNABLE_EQUAL, $line);
-					$lines[$idx] = $line;
+			foreach ($lines as $idx => $line) {
+				if (substr_count($line, $placeholder) > 0) {
+					$lines_with_objop[$block_count][] = $idx;
+				} else {
+					$block_count++;
 				}
 			}
-		}
 
-		$this->code = str_replace(self::ALIGNABLE_EQUAL, '', implode($this->new_line, $lines));
+			$i = 0;
+			foreach ($lines_with_objop as $group) {
+				if (1 == sizeof($group)) {
+					continue;
+				}
+				$i++;
+				$farthest_objop = 0;
+				foreach ($group as $idx) {
+					$farthest_objop = max($farthest_objop, strpos($lines[$idx], $placeholder));
+				}
+				foreach ($group as $idx) {
+					$line          = $lines[$idx];
+					$current_objop = strpos($line, $placeholder);
+					$delta         = abs($farthest_objop-$current_objop);
+					if ($delta > 0) {
+						$line        = str_replace($placeholder, str_repeat(' ', $delta).$placeholder, $line);
+						$lines[$idx] = $line;
+					}
+				}
+			}
+
+			$this->code = str_replace($placeholder, '', implode($this->new_line, $lines));
+		}
 
 		return $this->code;
 	}
