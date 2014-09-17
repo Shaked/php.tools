a0e3b01fb78d84211ccdf3e468ad3c23780e0048
Carlos C <uldericofilho@gmail.com>
2014-09-17 21:39:18 +0200

    Fix another case in which a do..while has two consecutive while's inside

--- codeFormatter.php
+++ codeFormatter.php
@@ -644,36 +644,32 @@ final class LeftAlignComment extends FormatterPass {
 
 final class MergeCurlyCloseAndDoWhile extends FormatterPass {
 	public function format($source) {
-		$this->tkns        = token_get_all($source);
-		$this->code        = '';
-		$in_do_while_stack = [];
+		$this->tkns = token_get_all($source);
+		$this->code = '';
 		while (list($index, $token) = each($this->tkns)) {
 			list($id, $text) = $this->get_token($token);
 			$this->ptr       = $index;
 			switch ($id) {
-				case T_DO:
-
-					$this->append_code($text, false);
-					$in_do_while_stack[] = 'DO';
+				case T_WHILE:
+					$str                   = $text;
+					list($pt_id, $pt_text) = $this->get_token($this->prev_token());
 					while (list($index, $token) = each($this->tkns)) {
 						list($id, $text) = $this->get_token($token);
 						$this->ptr       = $index;
-						$this->append_code($text, false);
-						if (ST_CURLY_OPEN === $id) {
+						$str .= $text;
+						if (
+							ST_CURLY_OPEN == $id ||
+							ST_COLON == $id ||
+							(ST_SEMI_COLON == $id && (T_COMMENT == $pt_id || T_DOC_COMMENT == $pt_id))
+						) {
+							$this->append_code($str, false);
+							break;
+						} elseif (ST_SEMI_COLON == $id && !(T_COMMENT == $pt_id || T_DOC_COMMENT == $pt_id)) {
+							$this->append_code($str);
 							break;
 						}
 					}
 					break;
-				case T_WHILE:
-					list($pt_id, $pt_text) = $this->get_token($this->prev_token());
-					if ($pt_id != ST_CURLY_CLOSE) {
-						$in_do_while_stack[] = 'WHILE';
-					}
-					$last_touched = end($in_do_while_stack);
-					if ('DO' == $last_touched && $this->is_token(ST_CURLY_CLOSE, true)) {
-						$this->append_code($text);
-						break;
-					}
 				default:
 					$this->append_code($text, false);
 					break;
