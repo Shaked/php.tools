77af0df62d94f5b73861438038565c3be8c603d3
Carlos C <uldericofilho@gmail.com>
2014-10-02 04:12:35 +0200

    Fixing bug in two commands in same line pass - should not touch literals and constants

--- TwoCommandsInSameLine.php
+++ TwoCommandsInSameLine.php
@@ -1,37 +1,37 @@
 <?php
 final class TwoCommandsInSameLine extends FormatterPass {
 	public function format($source) {
-		$lines = explode($this->new_line, $source);
-		foreach ($lines as $idx => $line) {
-			if (substr_count($line, ';') <= 1) {
-				continue;
+		$this->tkns = token_get_all($source);
+		$this->code = '';
+		while (list($index, $token) = each($this->tkns)) {
+			list($id, $text) = $this->get_token($token);
+			$this->ptr = $index;
+
+			switch ($id) {
+				case ST_SEMI_COLON:
+					$this->append_code($text, false);
+					if (!$this->has_ln_after() && $this->is_token([T_VARIABLE, T_STRING])) {
+						$this->append_code($this->new_line, false);
+					}
+					break;
+
+				case ST_PARENTHESES_OPEN:
+					$this->append_code($text, false);
+					while (list($index, $token) = each($this->tkns)) {
+						list($id, $text) = $this->get_token($token);
+						$this->ptr = $index;
+						$this->append_code($text, false);
+						if (ST_PARENTHESES_CLOSE == $id) {
+							break;
+						}
+					}
+					break;
+				default:
+					$this->append_code($text, false);
+					break;
+
 			}
-			$new_line           = '';
-			$ignore_stack       = 0;
-			$double_quote_state = false;
-			$single_quote_state = false;
-			$len                = strlen($line);
-			for ($i = 0; $i < $len; $i++) {
-				$char = substr($line, $i, 1);
-				if (ST_PARENTHESES_OPEN === $char || ST_PARENTHESES_OPEN === $char || ST_CURLY_OPEN === $char || ST_BRACKET_OPEN === $char) {
-					$ignore_stack++;
-				}
-				if (ST_PARENTHESES_CLOSE === $char || ST_CURLY_CLOSE === $char || ST_BRACKET_CLOSE === $char) {
-					$ignore_stack--;
-				}
-				if ('"' === $char) {
-					$double_quote_state = !$double_quote_state;
-				}
-				if ("'" === $char) {
-					$single_quote_state = !$single_quote_state;
-				}
-				$new_line .= $char;
-				if (!$single_quote_state && !$double_quote_state && 0 === $ignore_stack && ST_SEMI_COLON === $char && $i + 1 < $len) {
-					$new_line .= $this->new_line;
-				}
-			}
-			$lines[$idx] = $new_line;
 		}
-		return implode($this->new_line, $lines);
+		return $this->code;
 	}
 }
