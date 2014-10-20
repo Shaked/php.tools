f4ddeb034e20831c1d9c8a37689dc6951fa589ac
Carlos C <uldericofilho@gmail.com>
2014-10-20 18:48:09 +0200

    Fixing ShortArray Pass

--- codeFormatter.php
+++ codeFormatter.php
@@ -2453,40 +2453,41 @@ final class SettersAndGettersPass extends FormatterPass {
  * From PHP-CS-Fixer
  */
 class ShortArray extends FormatterPass {
+	const FOUND_ARRAY = 'array';
+	const FOUND_PARENTHESES = 'paren';
 	public function format($source) {
 		$this->tkns = token_get_all($source);
 		$this->code = '';
-		$paren_count = 0;
+		$found_paren = [];
 
 		while (list($index, $token) = each($this->tkns)) {
 			list($id, $text) = $this->get_token($token);
 			$this->ptr = $index;
 			switch ($id) {
 				case T_ARRAY:
-					while (list($index, $token) = each($this->tkns)) {
-						list($id, $text) = $this->get_token($token);
-						$this->ptr = $index;
-						if (T_WHITESPACE == $id) {
-							$this->append_code($text, false);
-							continue;
+					if ($this->is_token(array(ST_PARENTHESES_OPEN))) {
+						$found_paren[] = self::FOUND_ARRAY;
+						while (list($index, $token) = each($this->tkns)) {
+							list($id, $text) = $this->get_token($token);
+							$this->ptr = $index;
+							if (ST_PARENTHESES_OPEN == $id) {
+								$this->append_code('[', false);
+								break;
+							}
 						}
-
-						if (ST_PARENTHESES_OPEN == $id) {
-							++$paren_count;
-							$this->append_code(ST_BRACKET_OPEN, false);
-							continue;
-						} elseif (ST_PARENTHESES_CLOSE == $id) {
-							--$paren_count;
-							$this->append_code(ST_BRACKET_CLOSE, false);
-							continue;
-						}
-
-						if (0 == $paren_count) {
-							break;
-						}
+						break;
 					}
+				case ST_PARENTHESES_OPEN:
+					$found_paren[] = self::FOUND_PARENTHESES;
 					$this->append_code($text, false);
 					break;
+
+				case ST_PARENTHESES_CLOSE:
+					$pop_token = array_pop($found_paren);
+					if (self::FOUND_ARRAY == $pop_token) {
+						$this->append_code(']', false);
+						break;
+					}
 				default:
 					$this->append_code($text, false);
 					break;
