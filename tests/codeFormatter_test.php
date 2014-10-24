07c95a140d254c5f9ee7eebbf5588b431320a99e
Carlos C <uldericofilho@gmail.com>
2014-10-24 11:38:00 +0200

    Fixing false positive at travis-ci

--- tests/codeFormatter_test.php
+++ tests/codeFormatter_test.php
@@ -13,7 +13,12 @@
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 $start = microtime(true);
 $testEnv = true;
-include (realpath(__DIR__ . "/../codeFormatter.src.php"));
+$opt = getopt('v', ['verbose', 'deployed']);
+if (!isset($opt['deployed'])) {
+	include (realpath(__DIR__ . "/../codeFormatter.src.php"));
+} else {
+	include (realpath(__DIR__ . "/../codeFormatter.php"));
+}
 $cases = glob(__DIR__ . "/*.in");
 echo 'Running tests...', PHP_EOL;
 $brokenTests = [];
@@ -71,7 +76,6 @@ foreach ($cases as $caseIn) {
 echo PHP_EOL;
 echo 'Tests:', sizeof($cases) . PHP_EOL;
 echo 'Broken:', sizeof($brokenTests) . PHP_EOL;
-$opt = getopt('v', ['verbose']);
 if (isset($opt['v']) || isset($opt['verbose'])) {
 	foreach ($brokenTests as $caseOut => $test) {
 		file_put_contents($caseOut . '-got', $test);
