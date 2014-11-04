0a5a1023fafe76ac051153ffe3bc46a032571604
Carlos C <uldericofilho@gmail.com>
2014-11-04 10:41:42 +0100

    ExtraCommaInArray - array_shift -> array_pop

--- codeFormatter.php
+++ codeFormatter.php
@@ -1191,31 +1191,31 @@ final class ExtraCommaInArray extends FormatterPass {
 			switch ($id) {
 				case T_STRING:
 					if ($this->is_token(ST_PARENTHESES_OPEN)) {
-						array_unshift($context_stack, T_STRING);
+						$context_stack[] = T_STRING;
 					}
 					$this->append_code($text, false);
 					break;
 				case T_ARRAY:
 					if ($this->is_token(ST_PARENTHESES_OPEN)) {
-						array_unshift($context_stack, T_ARRAY);
+						$context_stack[] = T_ARRAY;
 					}
 					$this->append_code($text, false);
 					break;
 				case ST_PARENTHESES_OPEN:
 					if (isset($context_stack[0]) && $this->is_token(ST_PARENTHESES_CLOSE)) {
-						array_shift($context_stack);
+						array_pop($context_stack);
 					}
 					$this->append_code($text, false);
 					break;
 				case ST_PARENTHESES_CLOSE:
 					if (isset($context_stack[0])) {
-						array_shift($context_stack);
+						array_pop($context_stack);
 					}
 					$this->append_code($text, false);
 					break;
 				default:
-					if (isset($context_stack[0]) && T_ARRAY === $context_stack[0] && $this->is_token(ST_PARENTHESES_CLOSE)) {
-						array_shift($context_stack);
+					if (T_ARRAY === end($context_stack) && $this->is_token(ST_PARENTHESES_CLOSE)) {
+						array_pop($context_stack);
 						if (ST_COMMA === $id || T_END_HEREDOC === $id || T_COMMENT === $id || T_DOC_COMMENT === $id || !$this->has_ln_after()) {
 							$this->append_code($text, false);
 						} else {
