b79d4573bd77afe0d69690b678328d037f3ab03b
Carlos C <uldericofilho@gmail.com>
2014-11-14 14:19:51 +0100

    Fixing alignment error

--- AlignDoubleArrow.php
+++ AlignDoubleArrow.php
@@ -1,31 +1,60 @@
 <?php
 final class AlignDoubleArrow extends FormatterPass {
-	const ALIGNABLE_EQUAL = "\x2 EQUAL%d \x3";
+	const ALIGNABLE_EQUAL = "\x2 EQUAL%d.%d.%d \x3";// level.levelentracecounter.counter
 	public function format($source) {
 		$this->tkns = token_get_all($source);
 		$this->code = '';
 
-		$context_counter = 0;
-		$in_bracket = 0;
+		$level_counter = 0;
+		$level_entrance_counter = [];
+		$context_counter = [];
+
 		while (list($index, $token) = each($this->tkns)) {
 			list($id, $text) = $this->get_token($token);
 			$this->ptr = $index;
 			switch ($id) {
-				case T_FOREACH:
-				case ST_SEMI_COLON:
-				case T_ARRAY:
-					++$context_counter;
+				case ST_COMMA:
+					if (!$this->has_ln_after()) {
+						if (!isset($level_entrance_counter[$level_counter])) {
+							$level_entrance_counter[$level_counter] = 0;
+						}
+						if (!isset($context_counter[$level_counter][$level_entrance_counter[$level_counter]])) {
+							$context_counter[$level_counter][$level_entrance_counter[$level_counter]] = 0;
+						}
+						++$context_counter[$level_counter][$level_entrance_counter[$level_counter]];
+					}
+					$this->append_code($text, false);
+					break;
+				case T_DOUBLE_ARROW:
+					$this->append_code(
+						sprintf(
+							self::ALIGNABLE_EQUAL,
+							$level_counter,
+							$level_entrance_counter[$level_counter],
+							$context_counter[$level_counter][$level_entrance_counter[$level_counter]]
+						) . $text,
+						false
+					);
+					break;
+
+				case ST_PARENTHESES_OPEN:
+				case ST_BRACKET_OPEN:
+					++$level_counter;
+					if (!isset($level_entrance_counter[$level_counter])) {
+						$level_entrance_counter[$level_counter] = 0;
+					}
+					++$level_entrance_counter[$level_counter];
+					if (!isset($context_counter[$level_counter][$level_entrance_counter[$level_counter]])) {
+						$context_counter[$level_counter][$level_entrance_counter[$level_counter]] = 0;
+					}
+					++$context_counter[$level_counter][$level_entrance_counter[$level_counter]];
+
 					$this->append_code($text, false);
 					break;
 
-				case T_DOUBLE_ARROW:
-					$this->append_code(sprintf(self::ALIGNABLE_EQUAL, $context_counter) . $text, false);
-					break;
-
-				case ST_BRACKET_OPEN:
-					if ($this->is_token([T_DOUBLE_ARROW], true)) {
-						++$context_counter;
-					}
+				case ST_PARENTHESES_CLOSE:
+				case ST_BRACKET_CLOSE:
+					--$level_counter;
 					$this->append_code($text, false);
 					break;
 
@@ -35,49 +64,54 @@ final class AlignDoubleArrow extends FormatterPass {
 			}
 		}
 
-		for ($j = 0; $j <= $context_counter; ++$j) {
-			$placeholder = sprintf(self::ALIGNABLE_EQUAL, $j);
-			if (false === strpos($this->code, $placeholder)) {
-				continue;
-			}
-			if (1 === substr_count($this->code, $placeholder)) {
-				$this->code = str_replace($placeholder, '', $this->code);
-				continue;
-			}
-
-			$lines = explode($this->new_line, $this->code);
-			$lines_with_objop = [];
-			$block_count = 0;
-
-			foreach ($lines as $idx => $line) {
-				if (false !== strpos($line, $placeholder)) {
-					$lines_with_objop[$block_count][] = $idx;
-				} else {
-					++$block_count;
-					$lines_with_objop[$block_count] = [];
-				}
-			}
-
-			$i = 0;
-			foreach ($lines_with_objop as $group) {
-				++$i;
-				$farthest = 0;
-				foreach ($group as $idx) {
-					$farthest = max($farthest, strpos($lines[$idx], $placeholder));
-				}
-				foreach ($group as $idx) {
-					$line = $lines[$idx];
-					$current = strpos($line, $placeholder);
-					$delta = abs($farthest - $current);
-					if ($delta > 0) {
-						$line = str_replace($placeholder, str_repeat(' ', $delta) . $placeholder, $line);
-						$lines[$idx] = $line;
+		foreach ($context_counter as $level => $entrances) {
+			foreach ($entrances as $entrance => $context) {
+				for ($j = 0; $j <= $context; ++$j) {
+					$placeholder = sprintf(self::ALIGNABLE_EQUAL, $level, $entrance, $j);
+					if (false === strpos($this->code, $placeholder)) {
+						continue;
 					}
+					if (1 === substr_count($this->code, $placeholder)) {
+						$this->code = str_replace($placeholder, '', $this->code);
+						continue;
+					}
+
+					$lines = explode($this->new_line, $this->code);
+					$lines_with_objop = [];
+					$block_count = 0;
+
+					foreach ($lines as $idx => $line) {
+						if (false !== strpos($line, $placeholder)) {
+							$lines_with_objop[$block_count][] = $idx;
+						} else {
+							++$block_count;
+							$lines_with_objop[$block_count] = [];
+						}
+					}
+
+					$i = 0;
+					foreach ($lines_with_objop as $group) {
+						++$i;
+						$farthest = 0;
+						foreach ($group as $idx) {
+							$farthest = max($farthest, strpos($lines[$idx], $placeholder));
+						}
+						foreach ($group as $idx) {
+							$line = $lines[$idx];
+							$current = strpos($line, $placeholder);
+							$delta = abs($farthest - $current);
+							if ($delta > 0) {
+								$line = str_replace($placeholder, str_repeat(' ', $delta) . $placeholder, $line);
+								$lines[$idx] = $line;
+							}
+						}
+					}
+
+					$this->code = str_replace($placeholder, '', implode($this->new_line, $lines));
 				}
 			}
-
-			$this->code = str_replace($placeholder, '', implode($this->new_line, $lines));
 		}
+
 		return $this->code;
 	}
 }
