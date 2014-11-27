d16c61e399550536971cd67650f06911bdac04aa
Carlos C <uldericofilho@gmail.com>
2014-11-27 15:29:13 +0100

    Ability to skip intermediary tokens

--- refactor.php
+++ refactor.php
@@ -430,20 +430,55 @@ final class RefactorPass extends FormatterPass {
 			if ($id == $from[0][0]) {
 				$match = true;
 				$buffer = $text;
-				$i = 1;
 				for ($i = 1; $i < $from_size; ++$i) {
 					list($index, $token) = each($this->tkns);
 					$this->ptr = $index;
 					list($id, $text) = $this->get_token($token);
 					$buffer .= $text;
+					if ('/*skipUntil' == substr($from[$i][1], 0, 11)) {
+						$skip_call = $from[$i][1];
+						$stop_text = strtolower(trim(str_replace('skipUntil:', '', substr($text, 2, -2))));
+						++$i;
+						while (list($index, $token) = each($this->tkns)) {
+							$this->ptr = $index;
+							list($id, $text) = $this->get_token($token);
+							$buffer .= $text;
+							if ($id == $from[$i][0]) {
+								$tmp_i = $i;
+								$tmp_ptr = $this->ptr;
+								$s_match = true;
+								for ($tmp_i; $tmp_i < $from_size; ++$tmp_i, ++$tmp_ptr) {
+									if ($from[$tmp_i][0] != $this->tkns[$tmp_ptr][0]) {
+										$s_match = false;
+										break;
+									}
+								}
+								if ($s_match) {
+									break;
+								} else {
+									continue;
+								}
+							}
+							if (strtolower($text) == $stop_text) {
+								$match = false;
+								break 2;
+							}
+						}
+						continue;
+					}
 					if ($id != $from[$i][0]) {
 						$match = false;
 						break;
 					}
 				}
 				if ($match) {
-					$buffer = str_replace($from_str, $to_str, $buffer);
+					if (strpos($to_str, '/*skip*/')) {
+						$buffer = str_replace(explode($skip_call, $from_str), explode('/*skip*/', $to_str), $buffer);
+					} else {
+						$buffer = str_replace($from_str, $to_str, $buffer);
+					}
 				}
+
 				$this->append_code($buffer);
 			} else {
 				$this->append_code($text);
