14837df586ff7f26492d857b622faebcada65e4b
Carlos C <uldericofilho@gmail.com>
2014-11-27 18:32:23 +0100

    Fixes comments indentation within T_SWITCH
    Addresses dericofilho/sublime-phpfmt#57

--- codeFormatter.php
+++ codeFormatter.php
@@ -2302,6 +2302,8 @@ final class ReindentColonBlocks extends FormatterPass {
 		$switch_level = 0;
 		$switch_curly_count = [];
 		$switch_curly_count[$switch_level] = 0;
+		$is_next_case_or_default = false;
+		$touched_colon = false;
 		while (list($index, $token) = each($this->tkns)) {
 			list($id, $text) = $this->get_token($token);
 			$this->ptr = $index;
@@ -2313,6 +2315,7 @@ final class ReindentColonBlocks extends FormatterPass {
 				case T_SWITCH:
 					++$switch_level;
 					$switch_curly_count[$switch_level] = 0;
+					$touched_colon = false;
 					$this->append_code($text);
 					break;
 				case ST_CURLY_OPEN:
@@ -2332,13 +2335,22 @@ final class ReindentColonBlocks extends FormatterPass {
 					break;
 				case T_DEFAULT:
 				case T_CASE:
+					$touched_colon = false;
+					$this->append_code($text);
+					break;
+				case ST_COLON:
+					$touched_colon = true;
 					$this->append_code($text);
 					break;
 				default:
 					$has_ln = $this->has_ln($text);
 					if ($has_ln) {
-						$is_next_case_or_default = $this->right_token_is([T_CASE, T_DEFAULT]);
-						if (!$is_next_case_or_default && !$this->right_token_is([ST_CURLY_CLOSE, T_COMMENT, T_DOC_COMMENT])) {
+						$is_next_case_or_default = $this->right_useful_token_is([T_CASE, T_DEFAULT]);
+						if ($touched_colon && T_COMMENT == $id && $is_next_case_or_default) {
+							$this->append_code($text);
+						} elseif ($touched_colon && T_COMMENT == $id && !$is_next_case_or_default) {
+							$this->append_code($this->get_indent($switch_level) . $text);
+						} elseif (!$is_next_case_or_default && !$this->right_token_is([ST_CURLY_CLOSE, T_COMMENT, T_DOC_COMMENT])) {
 							$this->append_code($text . $this->get_indent($switch_level));
 						} else {
 							$this->append_code($text);
