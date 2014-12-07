56c7b57c639f50edd2ba5ea24db9ba0997ae7b3c
Carlos C <uldericofilho@gmail.com>
2014-12-07 20:15:52 +0100

    Ensuring method name propagation - PSR1

--- PSR1MethodNames.php
+++ PSR1MethodNames.php
@@ -4,6 +4,8 @@ final class PSR1MethodNames extends FormatterPass {
 		$this->tkns = token_get_all($source);
 		$this->code = '';
 		$found_method = false;
+
+		$method_replace_list = [];
 		while (list($index, $token) = each($this->tkns)) {
 			list($id, $text) = $this->get_token($token);
 			$this->ptr = $index;
@@ -15,10 +17,13 @@ final class PSR1MethodNames extends FormatterPass {
 				case T_STRING:
 					if ($found_method) {
 						$count = 0;
+						$orig_text = $text;
 						$tmp = ucwords(str_replace(['-', '_'], ' ', strtolower($text), $count));
 						if ($count > 0 && '' !== trim($tmp) && '_' !== substr($text, 0, 1)) {
 							$text = lcfirst(str_replace(' ', '', $tmp));
 						}
+
+						$method_replace_list[$orig_text] = $text;
 						$this->append_code($text);
 
 						$found_method = false;
@@ -26,6 +31,25 @@ final class PSR1MethodNames extends FormatterPass {
 					}
 				case ST_PARENTHESES_OPEN:
 					$found_method = false;
+				default:
+					$this->append_code($text);
+					break;
+			}
+		}
+
+		$this->tkns = token_get_all($this->code);
+		$this->code = '';
+		while (list($index, $token) = each($this->tkns)) {
+			list($id, $text) = $this->get_token($token);
+			$this->ptr = $index;
+			switch ($id) {
+				case T_STRING:
+					if (isset($method_replace_list[$text]) && $this->right_useful_token_is(ST_PARENTHESES_OPEN)) {
+
+						$this->append_code($method_replace_list[$text]);
+						break;
+					}
+
 				default:
 					$this->append_code($text);
 					break;
