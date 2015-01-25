ff7eaa46f8fabbeb045a72b483c45a069d3177aa
Carlos C <uldericofilho@gmail.com>
2015-01-25 15:31:12 +0100

    Rebuilding benchmark structure

---
+++ src/fmt_test.go
@@ -0,0 +1,46 @@
+package src
+
+import (
+	"os/exec"
+	"strconv"
+	"testing"
+)
+
+func TestNoop(t *testing.T) {
+
+}
+
+func BenchmarkHead(b *testing.B) {
+	benchmarkFmt(0, b)
+}
+func BenchmarkHeadMinus1(b *testing.B) {
+	benchmarkFmt(1, b)
+}
+func BenchmarkHeadMinus2(b *testing.B) {
+	benchmarkFmt(2, b)
+}
+func BenchmarkHeadMinus3(b *testing.B) {
+	benchmarkFmt(3, b)
+}
+func BenchmarkHeadMinus4(b *testing.B) {
+	benchmarkFmt(4, b)
+}
+func BenchmarkHeadMinus5(b *testing.B) {
+	benchmarkFmt(5, b)
+}
+
+func benchmarkFmt(commits int, b *testing.B) {
+	b.StopTimer()
+	exec.Command("git", "checkout", "master").Output()
+	exec.Command("git", "branch", "-D", "performance").Output()
+	exec.Command("git", "checkout", "-b", "performance").Output()
+	exec.Command("git", "reset", "--hard", "HEAD~"+strconv.Itoa(commits)).Output()
+	b.StartTimer()
+	for n := 0; n < b.N; n++ {
+		exec.Command("php", "test.php", "-v").Output()
+	}
+	b.StopTimer()
+	exec.Command("git", "checkout", "master").Output()
+	exec.Command("git", "branch", "-D", "performance").Output()
+	b.StartTimer()
+}
