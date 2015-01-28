555218897ca4ea95794783d96a8428671a9d78ad
Carlos C <uldericofilho@gmail.com>
2015-01-28 19:55:07 +0100

    Improving detection of short arrays

--- fmt.php
+++ fmt.php
@@ -1725,7 +1725,7 @@ final class ExtraCommaInArray extends FormatterPass {
 			$this->ptr = $index;
 			switch ($id) {
 				case ST_BRACKET_OPEN:
-					if (!$this->leftTokenIs([ST_BRACKET_CLOSE, T_STRING, T_VARIABLE, T_ARRAY_CAST])) {
+					if (!$this->leftTokenIs([ST_BRACKET_CLOSE, ST_CURLY_CLOSE, ST_PARENTHESES_CLOSE, T_STRING, T_VARIABLE, T_ARRAY_CAST])) {
 						$contextStack[] = self::ST_SHORT_ARRAY_OPEN;
 					} else {
 						$contextStack[] = ST_BRACKET_OPEN;
@@ -6867,7 +6867,7 @@ final class StripExtraCommaInArray extends AdditionalPass {
 			$this->ptr = $index;
 			switch ($id) {
 				case ST_BRACKET_OPEN:
-					if (!$this->leftTokenIs([ST_BRACKET_CLOSE, T_STRING, T_VARIABLE, T_ARRAY_CAST])) {
+					if (!$this->leftTokenIs([ST_BRACKET_CLOSE, ST_CURLY_CLOSE, ST_PARENTHESES_CLOSE, T_STRING, T_VARIABLE, T_ARRAY_CAST])) {
 						$contextStack[] = self::ST_SHORT_ARRAY_OPEN;
 					} else {
 						$contextStack[] = ST_BRACKET_OPEN;
--- src/Additionals/StripExtraCommaInArray.php
+++ src/Additionals/StripExtraCommaInArray.php
@@ -16,7 +16,7 @@ final class StripExtraCommaInArray extends AdditionalPass {
 			$this->ptr = $index;
 			switch ($id) {
 				case ST_BRACKET_OPEN:
-					if (!$this->leftTokenIs([ST_BRACKET_CLOSE, T_STRING, T_VARIABLE, T_ARRAY_CAST])) {
+					if (!$this->leftTokenIs([ST_BRACKET_CLOSE, ST_CURLY_CLOSE, ST_PARENTHESES_CLOSE, T_STRING, T_VARIABLE, T_ARRAY_CAST])) {
 						$contextStack[] = self::ST_SHORT_ARRAY_OPEN;
 					} else {
 						$contextStack[] = ST_BRACKET_OPEN;
