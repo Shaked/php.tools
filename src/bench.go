f0f89abad17092cd6f225d1a982871d317565aa3
Carlos C <uldericofilho@gmail.com>
2015-04-21 18:41:37 -0300

    Improving benchmark

--- src/bench.go
+++ src/bench.go
@@ -1,30 +1,64 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
-	"os"
+	"net/http"
 	"os/exec"
+	"runtime"
 	"strconv"
 	"strings"
 	"testing"
-	"text/tabwriter"
+	"time"
 )
+
+const TOTAL_COMMITS = 10
 
 func main() {
 	flag.Parse()
-	w := tabwriter.NewWriter(os.Stdout, 0, 8, 0, '\t', 0)
-	for commits := 10; commits > 0; commits-- {
+
+	result := runBench()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(
+			w,
+			TEMPLATE,
+			string(result),
+		)
+	})
+	fmt.Println("Listening")
+	if "darwin" == runtime.GOOS {
+		fmt.Println("opening browser")
+		go func() {
+			time.Sleep(3 * time.Second)
+			exec.Command("/usr/bin/open", "http://localhost:8080/").Output()
+		}()
+	}
+	http.ListenAndServe(":8080", nil)
+}
+
+func runBench() string {
+	var results [TOTAL_COMMITS][2]interface{}
+
+	for i := TOTAL_COMMITS - 1; i >= 0; i-- {
 		exec.Command("git", "checkout", "master").Output()
 		exec.Command("git", "branch", "-D", "performance").Output()
 		exec.Command("git", "checkout", "-b", "performance").Output()
-		exec.Command("git", "reset", "--hard", "HEAD~"+strconv.Itoa(commits)).Output()
+		exec.Command("git", "reset", "--hard", "HEAD~"+strconv.Itoa(i)).Output()
 		out, err := exec.Command("git", "log", "--pretty=oneline", "-n1").Output()
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Fprintf(w, "%s\t", strings.TrimSpace(string(out)))
+
+		fmt.Print(string(out))
+
+		commit := strings.Split(
+			strings.Replace(
+				strings.TrimSpace(string(out)), " ", ";", 1,
+			),
+			";",
+		)
 
 		b := testing.Benchmark(func(b *testing.B) {
 			for n := 0; n < b.N; n++ {
@@ -32,19 +66,72 @@ func main() {
 			}
 		})
 
-		fmt.Fprintln(
-			w,
-			b.N, "\t",
-			b.T, "\t",
+		results[i] = [2]interface{}{
+			// commit[0],
+			commit[1],
+			// b.N,
+			// b.T.Seconds(),
 			averagePerExecution(b.T.Nanoseconds(), int64(b.N)),
-		)
-
+		}
 		exec.Command("git", "checkout", "master").Output()
 		exec.Command("git", "branch", "-D", "performance").Output()
 	}
-	w.Flush()
+
+	b, err := json.MarshalIndent(results, "", "\t")
+	checkerr(err)
+
+	return string(b)
 }
 
 func averagePerExecution(t, n int64) float64 {
 	return float64(t/n) / float64(1e9)
+}
+
+const TEMPLATE = `
+  <html>
+  <head>
+    <script type="text/javascript"
+          src="https://www.google.com/jsapi?autoload={
+            'modules':[{
+              'name':'visualization',
+              'version':'1',
+              'packages':['corechart']
+            }]
+          }"></script>
+
+    <script type="text/javascript">
+      google.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = new google.visualization.DataTable();
+
+	data.addColumn('string',"hash");
+	// data.addColumn('string',"description");
+	// data.addColumn('number',"iterations");
+	// data.addColumn('number',"duration");
+	data.addColumn('number',"average");
+	data.addRows(%s)
+
+        var options = {
+          title: 'Benchmark',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="curve_chart" style="width: 900px; height: 500px"></div>
+  </body>
+</html>
+`
+
+func checkerr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
