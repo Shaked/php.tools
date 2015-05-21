00098ff0c3751a4eb0994320c0d45aa4443333d5
Carlos C <uldericofilho@gmail.com>
2015-05-21 16:51:06 +0200

    Optional baseline calculation

--- src/test.php
+++ src/test.php
@@ -14,7 +14,7 @@
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 $isHHVM = (false !== strpos(phpversion(), 'hhvm'));
 $shortTagEnabled = ini_get('short_open_tag');
-$opt = getopt('v', ['verbose', 'deployed', 'coverage', 'coveralls', 'testNumber:', 'stop']);
+$opt = getopt('v', ['verbose', 'deployed', 'coverage', 'coveralls', 'testNumber:', 'stop', 'baseline']);
 $isCoverage = isset($opt['coverage']) || isset($opt['coveralls']);
 $isCoveralls = isset($opt['coveralls']);
 if ($isCoverage) {
@@ -36,9 +36,12 @@ if (isset($opt['testNumber'])) {
 	}
 }
 
-echo 'Calculating baseline... ';
-$bogomips = bogomips();
-echo 'done', PHP_EOL;
+$bogomips = null;
+if (isset($opt['baseline'])) {
+	echo 'Calculating baseline... ';
+	$bogomips = bogomips();
+	echo 'done', PHP_EOL;
+}
 
 $start = microtime(true);
 $testEnv = true;
@@ -346,7 +349,13 @@ if (isset($opt['v']) || isset($opt['verbose'])) {
 		unlink($caseOut . '-got');
 	}
 }
-echo 'Took ', (microtime(true) - $start), ' at ', $bogomips, ' bogomips', PHP_EOL;
+
+echo 'Took ', (microtime(true) - $start);
+if (!is_null($bogomips)) {
+	echo ' at ', $bogomips, ' bogomips';
+}
+echo PHP_EOL;
+
 if ($isCoverage && !$isCoveralls) {
 	$writer = new PHP_CodeCoverage_Report_HTML();
 	$writer->process($coverage, './cover/');
