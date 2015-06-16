e65e5a81718044d5d9e7ab790c215942c2ee071c
Carlos C <uldericofilho@gmail.com>
2015-06-16 09:19:23 +0200

    Aim to simplify visibility update

--- src/Php2Go/Php2GoDecorator.php
+++ src/Php2Go/Php2GoDecorator.php
@@ -1,6 +1,7 @@
 <?php
 final class Php2GoDecorator {
 	public static function decorate(CodeFormatter $fmt) {
+		$fmt->enablePass('PSR2ModifierVisibilityStaticOrder');
 		$fmt->enablePass('TranslateNativeCalls');
 		$fmt->enablePass('UpdateVisibility');
 	}
